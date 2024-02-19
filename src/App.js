42c1ae47acf7be3d696f8e36865f921b344b98a6
Jake Hall <jake.hallslm@gmail.com>
2024-02-19 07:55:02 +0000

    add app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useEffect, useState } from "react";
+
+import { HashConnect, HashConnectConnectionState } from "hashconnect";
+import { LedgerId } from "@hashgraph/sdk";
+
+const appMetadata = {
+  name: "Hashgraph Hub Test",
+  description: "Testing Hashgraph Hub hashconnect",
+  icons: ["<Image url>"],
+  url: "<Dapp url>",
+};
 
 function App() {
+  const [pairingData, setPairingData] = useState(null);
+  const [connectionStatus, setConnectionStatus] = useState(
+    HashConnectConnectionState.Disconnected
+  );
+  const [hashconnect, setHashconnect] = useState(null);
+
+  useEffect(() => {
+    init();
+  }, []);
+
+  const init = async () => {
+    //create the hashconnect instance
+    const hashconnect = new HashConnect(
+      LedgerId.TESTNET,
+      process.env.REACT_APP_WALLETCONNECT_PROJECT_ID,
+      appMetadata,
+      true
+    );
+
+    //register events
+    hashconnect.pairingEvent.on((newPairing) => {
+      setPairingData(newPairing);
+    });
+
+    hashconnect.disconnectionEvent.on((data) => {
+      setPairingData(null);
+    });
+
+    hashconnect.connectionStatusChangeEvent.on((connectionStatus) => {
+      setConnectionStatus(connectionStatus);
+    });
+
+    //initialize
+    await hashconnect.init();
+
+    setHashconnect(hashconnect);
+  };
+
+  const disconnect = () => {
+    hashconnect.disconnect();
+  };
+
+  const connect = async () => {
+    await hashconnect.openPairingModal();
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello world</h1>
+      <p>Pairing data: {JSON.stringify(pairingData)}</p>
+      <p>Connection status: {connectionStatus}</p>
+
+      <button onClick={connect}>Open pairing modal</button>
+
+      <button onClick={disconnect}>Disconnect</button>
     </div>
   );
 }
